4eba756f6f379d0c786a2fb86ab59d2865d97a9b
thaynar96 <thaynarbo@gmail.com>
2021-09-01 14:34:36 -0300

    constructor function and instancing counters

---
+++ counter/app.js
@@ -0,0 +1,20 @@
+function Counter(element, value) {
+	this.counter = element;
+	this.value = value;
+	this.resetBtn = element.querySelector(".reset");
+	this.increaseBtn = element.querySelector(".increase");
+	this.decreaseBtn = element.querySelector(".decrease");
+	this.valueDOM = element.querySelector(".value");
+	this.valueDOM.textContent = this.value;
+}
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);
+
+function getElement(selection) {
+	const element = document.querySelector(selection);
+	if (element) {
+		return element;
+	}
+	throw new Error(`Please check "${selection}", no such element exists`);
+}
